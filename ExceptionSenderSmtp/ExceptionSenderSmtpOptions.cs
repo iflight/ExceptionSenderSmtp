c41da01e3fa533cfd291f2939aa0858115aa763a
Igor Vinogradov <vse.umrem@hotmail.com>
2017-12-17 23:59:55 +0300

    Init commit. ExceptionSenderSmtpTask

---
+++ ExceptionSenderSmtp/ExceptionSenderSmtpOptions.cs
@@ -0,0 +1,37 @@
+﻿namespace ExeptionsSenderSmpt
+{
+    using Logging.ExceptionSender;
+
+    public class ExceptionSenderSmtpOptions : ExceptionSenderOptions
+    {
+        /// <summary>
+        /// Host your smtp server
+        /// </summary>
+        public string SmtpHost { get; set; }
+
+        /// <summary>
+        /// Port of smtp server
+        /// </summary>
+        public int SmtpPort { get; set; }
+
+        /// <summary>
+        /// Login of smtp server
+        /// </summary>
+        public string SmtpLogin { get; set; }
+
+        /// <summary>
+        /// Password of smtp server
+        /// </summary>
+        public string SmtpPassword { get; set; }
+
+        /// <summary>
+        /// Sender email ('from' in messages)
+        /// </summary>
+        public string From { get; set; }
+
+        /// <summary>
+        /// Target (admin) email(s) ('to' in messages)
+        /// </summary>
+        public string[] To { get; set; }
+    }
+}
---
+++ ExceptionSenderSmtp/ExceptionSenderSmtpTask.cs
@@ -0,0 +1,58 @@
+﻿namespace ExeptionsSenderSmpt
+{
+    using System.IO;
+    using System.Net;
+    using Logging.ExceptionSender;
+    using MailKit.Net.Smtp;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+    using MimeKit;
+    using RecurrentTasks;
+
+    public class ExceptionSenderSmtpTask : ExceptionSenderTask
+    {
+
+        private ExceptionSenderSmtpOptions options;
+
+        public ExceptionSenderSmtpTask(
+            ILogger<ExceptionSenderSmtpTask> logger,
+            IOptions<ExceptionSenderSmtpOptions> options,
+            IHostingEnvironment hostingEnvironment)
+            : base(logger, options.Value, hostingEnvironment)
+        {
+            this.options = options.Value;
+        }
+
+        protected override void Send(ITask currentTask, string text, FileInfo logFile)
+        {
+            SmtpClient client = new SmtpClient();
+            client.Connect(this.options.SmtpHost, this.options.SmtpPort, true);
+            client.Authenticate(new NetworkCredential(this.options.SmtpLogin, this.options.SmtpPassword));
+
+            MimeMessage mailMessage = new MimeMessage();
+
+            mailMessage.Sender = new MailboxAddress(this.options.From);
+
+            mailMessage.Subject = text.Substring(0, text.IndexOf("\r\n"));
+
+            foreach (var to in options.To)
+            {
+                mailMessage.To.Add(new MailboxAddress(to));
+            }
+
+            var bodyBuilder = new BodyBuilder();
+
+            bodyBuilder.TextBody = text;
+
+            if (logFile != null && logFile.Exists)
+            {
+                bodyBuilder.Attachments.Add(logFile.FullName);
+            }
+
+            mailMessage.Body = bodyBuilder.ToMessageBody();
+
+            client.Send(mailMessage);
+        }
+    }
+}
